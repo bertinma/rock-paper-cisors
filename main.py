98f0ccbf42ef824d36845697096efd2e6c2fd3a7
bertinma <bertin_ma@icloud.com>
2023-02-15 18:16:46 +0100

    feat : print results + update image

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QLineEdit
 import time
 import random
+from pathlib import Path
 
 class RockPaperScissorsWindow(QWidget):
     def __init__(self):
@@ -85,57 +86,95 @@ class GameWindow(QWidget):
         self.player2_input.setText("Computer")
 
         # Create a list of image paths for rock, paper, and scissors
-        self.images = ["rock.png", "paper.png", "scissors.png"]
+        self.images = ["rock.png", "scissors.png", "paper.png"]
         self.image_index = 0
-
+        self.image_index_computer = None 
         # display rotativly the three images until a keyboard event 
         # is triggered
         # Display them on left and on the right of the window 
         # (one for each player)
-        self.quit_loop = False
         self.display_image()
 
     def display_image(self):
         self.image1 = QLabel(self)
         self.image2 = QLabel(self)
-        if not self.quit_loop:
-            timer = QTimer(self)
-            timer.timeout.connect(self.updateImage)
-            timer.start(.3*1000)
-            self.updateImage()
+        self.timer = QTimer(self)
+        self.textResult = QLabel(self)
+        self.timer.timeout.connect(self.updateImage)
+        self.timer.start(300)
+        self.updateImage()
 
     def updateImage(self):
         pixmap = QPixmap("images/" + self.images[self.image_index])
         pixmap = pixmap.scaled(200, 200, Qt.KeepAspectRatio)
         self.image1.setPixmap(pixmap)
         self.image1.move(75, 250)
-        pixmap = QPixmap("images/" + self.images[self.image_index + 1 if self.image_index < 2 else 0])
+        if self.image_index_computer is None:
+            self.image_index_computer = self.image_index + 1 if self.image_index < 2 else 0
+            self.image_index += 1
+            self.image_index %= 3
+        pixmap = QPixmap("images/" + self.images[self.image_index_computer])
         pixmap = pixmap.scaled(200, 200, Qt.KeepAspectRatio)
         self.image2.setPixmap(pixmap)
         self.image2.move(475, 250)
-        self.image_index += 1
-        self.image_index %= 3
+        self.image_index_computer = None
 
 
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_Return:
             self.player1_input.setReadOnly(True)
             self.player2_input.setReadOnly(True)
-        if event.key() in [Qt.Key_Escape, Qt.Key_Q, Qt.Key_S, Qt.Key_D]:
-            self.quit_loop = True 
-        if event.key() == Qt.Key_Q:
-            self.image_index = 0
-        elif event.key() == Qt.Key_S:
-            self.image_index = 1
-        elif event.key() == Qt.Key_D:
-            self.image_index = 2
+        elif event.key() == Qt.Key_M:
+            self.player1_input.setReadOnly(False)
+            self.player2_input.setReadOnly(False)
+        else:
+            # select random in between 0 and 2 included
+            self.image_index_computer = random.randint(0, 2)
+            pcr_str = " ".join([Path(self.images[i]).stem for i in range(3)])
+            print(pcr_str)
+            print(f"Key pressed:  {event.text()} {Path(self.images[self.image_index]).stem}")
+            print(f'Computer choice: {self.image_index_computer} {Path(self.images[self.image_index_computer]).stem}')
+            if event.key() in [Qt.Key_Escape, Qt.Key_Q, Qt.Key_S, Qt.Key_D]:
+                self.timer.disconnect()
+                print(f"Quit loop !")
+            if event.key() == Qt.Key_Q:
+                self.image_index = 0
+            elif event.key() == Qt.Key_S:
+                self.image_index = 1
+            elif event.key() == Qt.Key_D:
+                self.image_index = 2
+            self.displayResults()
+            self.updateScores()
+            self.updateImage()
+            time.sleep(3)
+            self.show()
+            self.timer.timeout.connect(self.updateImage)
 
-        # select random in between 0 and 2
-        self.image_index_computer = random.randint(0, 2)
+    def displayResults(self):
+        # Display the results of the game
+        self.textResult.move(250, 480)
+        self.textResult.setStyleSheet("background: red;")
+        print(f"image_index: {self.image_index}")
+        print(f"image_index_computer: {self.image_index_computer}")
 
+        if self.image_index == self.image_index_computer:
+            self.textResult.setText("Draw")
+            print("Draw")
+        elif (self.image_index + 1) % 3 == self.image_index_computer:
+            self.textResult.setText(f" {self.player1_input.text()} wins")
+            print(f" {self.player1_input.text()} wins")
+            # write text in green color
+            self.textResult.setStyleSheet("color: green;")
+
+        else:
+            self.textResult.setText(f" {self.player2_input.text()} wins")
+            print(f" {self.player2_input.text()} wins")
+            # write text in red color
+            self.textResult.setStyleSheet("color: red;")
+        print("\n\n\n")
         
 
-    def update_scores(self):
+    def updateScores(self):
         # Update the scores of the players
         pass
 
